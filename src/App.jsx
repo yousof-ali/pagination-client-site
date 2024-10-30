38f02a66fa339378da03b43c67bb8435f9229294
yousof-ali <yousofali293@gmail.com>
2024-10-30 19:51:01 +0600

    simple page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Shop from './components/Shop/Shop'
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,17 +3,31 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    const {count} = useLoaderData();
+    const [itemsPerPage,setitemsPerPage] = useState(10);
+    const [currentPage,setCurrentPage] = useState(0);
+    
+    const numberOfPages = Math.ceil(count/itemsPerPage);
+    console.log(numberOfPages);
+    
+    // const myPageArray = []
+    // for(let i = 1 ; i<numberOfPages+1; i++){
+    //       myPageArray.push(i);
+    // }
+    // console.log(myPageArray);
+    const myPageArray = [...Array(numberOfPages).keys()];
+    console.log(myPageArray);
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage,itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -34,6 +48,24 @@ const Shop = () => {
         // step 5: set the cart
         setCart(savedCart);
     }, [products])
+
+    const handleitemsPerPage = (e) => {
+        console.log(e.target.value);
+        const val = parseInt(e.target.value)
+        setitemsPerPage(val)
+        setCurrentPage(0);
+    
+    }
+    const handlePrevButton = () =>{
+        if(currentPage>0){
+            setCurrentPage(currentPage-1);
+        }
+    }
+    const handleNextButton = () => {
+        if(currentPage<myPageArray.length-1){
+            setCurrentPage(currentPage+1)
+        }
+    }
 
     const handleAddToCart = (product) => {
         // cart.push(product); '
@@ -81,6 +113,22 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div className='pagination'>
+                <button onClick={handlePrevButton}>Prev</button>
+                {
+                    myPageArray.map(singlePage => <button
+                    className={singlePage == currentPage&&"selected"}
+                    onClick={()=>setCurrentPage(singlePage)}
+                    >{singlePage}</button>)
+                }
+                <button onClick={handleNextButton}>Next</button>
+                <select onChange={handleitemsPerPage} value={itemsPerPage} id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
             </div>
         </div>
     );
